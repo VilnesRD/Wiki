a2738c33fce75adf155442ee0ad0a260bd6bc0e6
Dmitry Rodichev <rdp9000@gmail.com>
2023-03-09 18:35:23 +0700

    upd test

---
+++ src/test/java/wikiTests/mobileTests/androidTests/openArticle.java
@@ -0,0 +1,29 @@
+package wikiTests.mobileTests.androidTests;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import wikiTests.mobileTests.TestBase;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.back;
+import static io.appium.java_client.AppiumBy.className;
+import static io.appium.java_client.AppiumBy.id;
+import static io.qameta.allure.Allure.step;
+
+public class openArticle extends TestBase {
+    @Test
+    @DisplayName("Открытие статьи")
+    void openTopic() {
+        back();
+        step("Type search", () -> {
+            $(id("org.wikipedia:id/search_container")).click();
+            $(id("org.wikipedia:id/search_src_text")).setValue("BrowserStack");
+        });
+        step("Open topic", () ->
+                $(id("org.wikipedia:id/page_list_item_title"))).click();
+        step("Check topic's article", () ->
+                $(className("android.widget.TextView")).shouldHave(text("BrowserStack")));
+    }
+}
+
