8ea3dd3f3e8b956204fa00926cada49340033483
Dmitry Rodichev <rdp9000@gmail.com>
2023-03-09 17:45:00 +0700

    upd test

--- src/test/java/wikiTests/mobileTests/androidTests/openSettings.java
+++ src/test/java/wikiTests/mobileTests/androidTests/openSettings.java
@@ -8,16 +8,15 @@ import wikiTests.mobileTests.TestBase;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.back;
+import static io.appium.java_client.AppiumBy.id;
 
 public class openSettings extends TestBase {
     @Test
     @Tag("android")
     @DisplayName("Открытие страницы настроек")
     void openSettingPage() {
-        back();
-        $(AppiumBy.id("org.wikipedia.alpha:id/nav_more_container")).click();
-        $(AppiumBy.id("org.wikipedia.alpha:id/main_drawer_settings_container")).click();
+        $(id("org.wikipedia.alpha:id/menu_overflow_button")).click();
+        $(id("org.wikipedia.alpha:id/explore_overflow_settings")).click();
         $(AppiumBy.className("android.widget.TextView")).shouldHave(text("Settings"));
     }
 }
