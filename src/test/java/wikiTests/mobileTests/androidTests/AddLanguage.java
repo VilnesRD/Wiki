5ae58aa36232ac489f4f49ec151a47a0275c103f
Dmitry Rodichev <rdp9000@gmail.com>
2023-03-09 17:05:11 +0700

    upd test

--- src/test/java/wikiTests/mobileTests/androidTests/AddLanguage.java
+++ src/test/java/wikiTests/mobileTests/androidTests/AddLanguage.java
@@ -5,13 +5,14 @@ import io.qameta.allure.Owner;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import wikiTests.mobileTests.TestBase;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 
-public class AddLanguage {
+public class AddLanguage extends TestBase {
     @Owner("Rodichev")
     @Test
     @Tag("android")
